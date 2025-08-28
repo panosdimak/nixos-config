c50762e3102a1e2b693907a2ca9c1a7ddd926d6f
Panos Dimakakos <panosdimak13@gmail.com>
2025-08-29 00:30:31 +0300

    Change nixpkgs branch to 25.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs (stable)
-    nixpkgs.url = "github:NixOS/nixpkgs/005433b9";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
 
     # Home Manager, following the same nixpkgs
     # home-manager.url = "github:nix-community/home-manager/release-25.05";
