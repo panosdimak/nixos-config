421332fa26a3cf3d6664ff0a35c6fe77469d8448
Panos Dimakakos <panosdimak13@gmail.com>
2025-08-29 00:33:10 +0300

    Change GNOME config options for 25.05

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -1,6 +1,6 @@
 { ... }:
 
 {
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
 }
