60833877d90d49e530f52367a07fa033e94cbf0a
Panos Dimakakos <panosdimak13@gmail.com>
2025-08-29 01:38:49 +0300

    move env variables to system module

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -10,16 +10,4 @@
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-
-  environment.variables = {
-    LIBVA_DRIVER_NAME = "nvidia";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    GBM_BACKEND = "nvidia_drm";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    QT_QPA_PLATFORM = "wayland";
-    MOZ_ENABLE_WAYLAND = "1";
-
-    EDITOR = "nvim";
-    VISUAL = "nvim";
-  };
 }
--- modules/system.nix
+++ modules/system.nix
@@ -9,4 +9,16 @@
   };
 
   nixpkgs.config.allowUnfree = true;
+
+  environment.variables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    GBM_BACKEND = "nvidia_drm";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    QT_QPA_PLATFORM = "wayland";
+    MOZ_ENABLE_WAYLAND = "1";
+
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+  };
 }
