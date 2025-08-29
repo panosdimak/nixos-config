ba2c33e212e3b9aa335eb96a6c83607375761d8f
Panos Dimakakos <panosdimak13@gmail.com>
2025-08-29 20:32:40 +0300

    update gnome options

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -1,6 +1,6 @@
 { ... }:
 
 {
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
 }
