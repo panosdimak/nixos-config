801cfe9b3cc7b372db5e6304786d17b01ef4e1d9
Panos Dimakakos <panosdimak13@gmail.com>
2025-08-29 20:23:59 +0300

    add home manager module

--- flake.nix
+++ flake.nix
@@ -2,13 +2,14 @@
   description = "My NixOS configuration";
 
   inputs = {
-    # Nixpkgs (stable)
+    # Base Nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # Home Manager, following the same nixpkgs
-    # home-manager.url = "github:nix-community/home-manager/release-25.05";
-    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # Home Manager, following nixpkgs
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Extra flakes
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,24 +20,29 @@
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-
         modules = [
           ./hosts/desktop.nix
 
           # Optional: enable Home Manager as a NixOS module
-          # home-manager.nixosModules.home-manager
-          # {
-          #   home-manager.useGlobalPkgs = true;
-          #   home-manager.useUserPackages = true;
-          #   home-manager.users. = import ./home/potis.nix;
-          # }
+          home-manager.nixosModules.home-manager
           {
-              environment.systemPackages = [
-                zen-browser.packages.x86_64-linux.twilight
-              ];
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.potis = import ./home/potis.nix;
+            home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
       };
     };
+    
+    # Standalone Home Manager configs
+    homeConfigurations.potis = home-manager.lib.homeManagerConfiguration {
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      modules = [ 
+        ./home/potis.nix
+      ];
+      extraSpecialArgs = { inherit inputs; };
+    };
+
   };
 }
