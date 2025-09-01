6de1ecdf8409b59ae350fa4837050295d9d1d753
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-01 23:32:32 +0300

    add musnix

--- flake.nix
+++ flake.nix
@@ -9,21 +9,23 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # Extra flakes
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Musnix
+    musnix.url = "github:musnix/musnix";
+    musnix.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Zen browser
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, zen-browser, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, musnix, zen-browser, ... }@inputs: {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/desktop.nix
 
-          # Optional: enable Home Manager as a NixOS module
+          # Home Manager as a NixOS module
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
@@ -31,6 +33,9 @@
             home-manager.users.panos = import ./home/panos.nix;
             home-manager.extraSpecialArgs = { inherit inputs; };
           }
+
+          # Musnix module
+          musnix.nixosModules.musnix
         ];
       };
     };
