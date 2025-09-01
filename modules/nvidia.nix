c43ea997ad75049f0fee2ee0e6d5743de93214fb
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-01 23:52:46 +0300

    split environment variables per module

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -10,4 +10,13 @@
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+
+  environment.variables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    GBM_BACKEND = "nvidia_drm";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    QT_QPA_PLATFORM = "wayland";
+    MOZ_ENABLE_WAYLAND = "1";
+  };
 }
--- modules/system.nix
+++ modules/system.nix
@@ -13,13 +13,6 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.variables = {
-    LIBVA_DRIVER_NAME = "nvidia";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    GBM_BACKEND = "nvidia_drm";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    QT_QPA_PLATFORM = "wayland";
-    MOZ_ENABLE_WAYLAND = "1";
-
     EDITOR = "nvim";
     VISUAL = "nvim";
   };
