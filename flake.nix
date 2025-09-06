e1e9597ff6677ef9e4681e432c2a2da235d8a7a4
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-06 23:16:36 +0300

    correct plasma-manager home module reference in flake.nix

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.sharedModules = [
-              inputs.plasma-manager.homeManagerModules."plasma-manager"
+              inputs.plasma-manager.homeModules."plasma-manager"
             ];
             home-manager.users.panos = import ./home/panos.nix;
             home-manager.extraSpecialArgs = { inherit inputs; };
