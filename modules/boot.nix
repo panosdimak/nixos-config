5ef8c7ae04be80d834dc46919b761cef0a4a4b42
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-08 06:36:11 +0300

    boot: enable systemd initrd and zswap (lz4)

--- modules/boot.nix
+++ modules/boot.nix
@@ -8,6 +8,15 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 3;
 
+  boot.initrd.systemd.enable = true;
+  boot.initrd.kernelModules = [ "lz4" ];
+  boot.plymouth.enable = false;
+
+  boot.kernelParams = [ 
+    "usbcore.autosuspend=-1" 
+    "zswap.enabled=1"
+    "zswap.compressor=lz4"
+  ];
+
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = [ "usbcore.autosuspend=-1" ];
 }
