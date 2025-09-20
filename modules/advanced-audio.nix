1777a43c05bc51a4711fcf182ec805cbed200a97
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-20 16:34:37 +0300

    refactor: move advanced audio configuration to a separate file and clean up audio module

---
+++ modules/advanced-audio.nix
@@ -0,0 +1,41 @@
+{ ... }:
+
+{
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+
+    extraConfig = {
+      # Keep the graph sane and consistent
+      pipewire."10-lowlatency" = {
+        "context.properties" = {
+          "default.clock.allowed-rates" = [ 48000 ];
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 256;
+          "default.clock.min-quantum" = 256;
+          "default.clock.max-quantum" = 256;
+        };
+      };
+
+      jack."10-lowlatency" = {
+        "jack.properties" = {
+          "node.latency" = "256/48000";
+          "node.lock-quantum" = true;
+        };
+      };
+    };
+  };
+
+  musnix = {
+    enable = true;
+    rtirq.enable = true;
+    rtcqs.enable = true;
+  };
+}
--- modules/audio.nix
+++ modules/audio.nix
@@ -11,31 +11,5 @@
     pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
-
-    extraConfig = {
-      # Keep the graph sane and consistent
-      pipewire."10-lowlatency" = {
-        "context.properties" = {
-          "default.clock.allowed-rates" = [ 48000 ];
-          "default.clock.rate" = 48000;
-          "default.clock.quantum" = 256;
-          "default.clock.min-quantum" = 256;
-          "default.clock.max-quantum" = 256;
-        };
-      };
-
-      jack."10-lowlatency" = {
-        "jack.properties" = {
-          "node.latency" = "256/48000";
-          "node.lock-quantum" = true;
-        };
-      };
-    };
-  };
-
-  musnix = {
-    enable = true;
-    rtirq.enable = true;
-    rtcqs.enable = true;
   };
 }
