8627fe9cb4686bcf041d470f7ed68ef72195d50b
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-20 17:38:48 +0300

    refactor: move gaming-related configurations to a separate file

--- modules/programs/common.nix
+++ modules/programs/common.nix
@@ -1,10 +1,7 @@
 { pkgs, ... }:
 
 {
-  programs.kdeconnect.enable = true;
-  programs.steam.enable = true;
-  # programs.hyprland.enable = true;
-
+  # Safe, common tools
   services.flatpak.enable = true;
 
   environment.systemPackages = with pkgs; [
@@ -13,9 +10,6 @@
     alejandra
     usbutils
     uhubctl
-    yabridge
-    yabridgectl
-    reaper
   ];
 
   fonts.packages = with pkgs; [
