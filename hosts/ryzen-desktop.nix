bd15a44bd22f71d15282b3c00a9e8f4118c73aaf
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-20 17:45:44 +0300

    reorganize import modules

--- hosts/ryzen-desktop.nix
+++ hosts/ryzen-desktop.nix
@@ -6,19 +6,25 @@
 
     ../modules/boot.nix
     ../modules/hardware.nix
+    ../modules/amd.nix
     ../modules/networking.nix
     ../modules/locale.nix
     ../modules/system.nix
     ../modules/security.nix
     ../modules/desktop/plasma.nix
-    ../modules/desktop/common.nix
-    ../modules/hyprland/hyprland.nix
+    ../modules/desktop/portals-plasma.nix
+    ../modules/desktop/portals-hyprland.nix
+    ../modules/hyprland.nix
     ../modules/nvidia.nix
+    ../modules/graphics-32bit.nix
     ../modules/advanced-audio.nix
     ../modules/users/panos.nix
     ../modules/programs/common.nix
+    ../modules/programs/gaming.nix
     ../modules/stylix.nix
   ];
 
   system.stateVersion = "25.05";
+  networking.hostName = "ryzen-desktop";
+
 }
