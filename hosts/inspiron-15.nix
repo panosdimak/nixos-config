f1e3cb29d49759c7a1ab58c361496732f44c9488
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-20 18:18:13 +0300

    add: create inspiron-15 configuration

---
+++ hosts/inspiron-15.nix
@@ -0,0 +1,35 @@
+{
+  imports = [
+    ./inspiron-15-hardware.nix
+
+    ../modules/boot.nix
+    ../modules/hardware.nix
+    ../modules/intel.nix
+    ../modules/networking.nix
+    ../modules/locale.nix
+    ../modules/system.nix
+    ../modules/portals.nix
+    ../modules/hyprland.nix
+    ../modules/audio.nix
+    ../modules/users/panos.nix
+    ../modules/programs/common.nix
+    ../modules/stylix.nix
+    ../modules/display-manager.nix
+  ];
+
+  system.stateVersion = "25.05";
+  networking.hostName = "inspiron-15";
+
+  # Choose display manager for this host
+  profiles.displayManager = {
+    dm = "gdm";
+    wayland = true;
+  };
+
+  # Choose portals for this host
+  profiles.portals = {
+    session = "hyprland";
+    enableGtkFallback = true;
+  };
+
+}
