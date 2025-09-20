d69f3678264ba68f1ca967be554262ede820c850
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-20 18:18:25 +0300

    refactor: consolidate portal configurations and add display manager settings

--- hosts/ryzen-desktop.nix
+++ hosts/ryzen-desktop.nix
@@ -12,8 +12,7 @@
     ../modules/system.nix
     ../modules/security.nix
     ../modules/desktop/plasma.nix
-    ../modules/desktop/portals-plasma.nix
-    ../modules/desktop/portals-hyprland.nix
+    ../modules/portals.nix
     ../modules/hyprland.nix
     ../modules/nvidia.nix
     ../modules/graphics-32bit.nix
@@ -22,9 +21,22 @@
     ../modules/programs/common.nix
     ../modules/programs/gaming.nix
     ../modules/stylix.nix
+  ../modules/display-manager.nix
   ];
 
   system.stateVersion = "25.05";
   networking.hostName = "ryzen-desktop";
+
+  # Choose display manager for this host
+  profiles.displayManager = {
+    dm = "sddm";
+    wayland = true;
+  };
+
+  # Choose portals for this host
+  profiles.portals = {
+    session = "hyprland";
+    enableGtkFallback = true;
+  };
 
 }
