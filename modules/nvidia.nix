78985a71177c50cb99afe5a03fb5705c76f79acd
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-26 19:37:14 +0300

    fix: update nvidia package to production and clean up environment variables

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -8,15 +8,10 @@
     powerManagement.enable = true;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
-  environment.variables = {
-    LIBVA_DRIVER_NAME = "nvidia";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    GBM_BACKEND = "nvidia_drm";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    QT_QPA_PLATFORM = "wayland";
-    MOZ_ENABLE_WAYLAND = "1";
-  };
+  boot.kernelParams = [ 
+    "nvidia_drm.modeset=1"
+  ];
 }
