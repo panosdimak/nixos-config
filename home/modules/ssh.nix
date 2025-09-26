395e50e0bcc9f6f161ddafaf4932df6a8f91747c
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-26 13:21:42 +0300

    refactor: use correct nix ssh options

--- home/modules/ssh.nix
+++ home/modules/ssh.nix
@@ -3,10 +3,25 @@
 {
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host github.com
-        User git
-        IdentityFile ~/.ssh/id_ed25519
-    '';
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "confirm"; # adds keys but asks for approval
+      compression = false;
+      serverAliveInterval = 60;
+      serverAliveCountMax = 3;
+      hashKnownHosts = true;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      
+      # Multiplexing for fast Git/SSH connections
+      controlMaster = "auto";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "10m"; # keep master open for 10 minutes
+    };
+
+    matchBlocks."github.com" = {
+      user = "git";
+      identityFile = "~/.ssh/id_ed25519";
+    };
   };
 }
