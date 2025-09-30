c6bb03d4b16957f906514af3ffc6d5e8692f8222
Panos Dimakakos <panosdimak13@gmail.com>
2025-09-30 03:14:46 +0300

    fix: remove fuzzel configuration from stylix module

--- home/modules/stylix.nix
+++ home/modules/stylix.nix
@@ -1,22 +1,6 @@
 { lib, ... }:
 {
-  # Enable the apps so Stylix can write their settings
-  programs = {
-    # Required so Stylix can inject its theme into fuzzel's config
-    fuzzel = {
-      enable = true;
-      settings = {
-        main = {
-          font = lib.mkForce "SFMono Nerd Font:size=14.5";
-          dpi-aware = "yes";
-        };
-        border = {
-          selection-radius = 10;
-        };
-      };
-    };
-    vesktop.enable = true;
-  };
+  programs.vesktop.enable = true;
 
   # Enable Home-Manager-only Stylix targets
   stylix.targets = {
