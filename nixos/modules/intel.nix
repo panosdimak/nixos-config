7fdd1160d8f6e2c3310b4545089598d080e15f9b
Panos Dimakakos <panosdimak13@gmail.com>
2025-10-05 15:09:29 +0300

    feat: add Intel GPU power management and additional graphics packages

--- nixos/modules/intel.nix
+++ nixos/modules/intel.nix
@@ -1,7 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # Intel CPU microcode updates
   hardware.cpu.intel.updateMicrocode = true;
 
+  # Intel GPU power management and driver options
+  boot.kernelParams = [
+    "i915.enable_fbc=1"        # Enable framebuffer compression
+    "i915.enable_psr=2"        # Enable Panel Self Refresh
+    "i915.fastboot=1"          # Enable fastboot
+  ];
+
+  # Additional Intel graphics packages
+  hardware.graphics.extraPackages = with pkgs; [
+    intel-media-driver
+    intel-vaapi-driver
+    libvdpau-va-gl
+  ];
 }
