eb12114c3d4a2cee7b27732164b35336ee34860c
Panos Dimakakos <panosdimak13@gmail.com>
2025-10-06 14:11:12 +0300

    fix: add cursor style setting to ghostty configuration

--- home/modules/ghostty.nix
+++ home/modules/ghostty.nix
@@ -4,10 +4,9 @@
   programs.ghostty = { 
     enable = true;
     settings = {
-    gtk-single-instance = true;
-    background-opacity = 0.8;
-    background-blur = true;
-    cursor-style = "bar";
+      gtk-single-instance = true;
+      background-opacity = 0.8;
+      background-blur = true;
     };
   };
 }
