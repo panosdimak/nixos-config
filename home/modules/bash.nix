8e85734e61158583d90cc380f455bab20c543025
Panos Dimakakos <panosdimak13@gmail.com>
2025-10-06 14:33:00 +0300

    fix: enhance bash configuration with improved history settings and shell aliases

--- home/modules/bash.nix
+++ home/modules/bash.nix
@@ -3,12 +3,35 @@
 {
     programs.bash = {
     enable = true;
-    initExtra = ''eval "$(starship init bash)"'';
+
+    historyControl = [ "ignoredups" "ignorespace" ];
+    historySize = 10000;
+    historyFileSize = 20000;
+
+    initExtra = ''
+      eval "$(starship init bash)"
+      eval "$(fzf --bash)"
+    '';
+
     shellAliases = {
       nixbuildflake = "sudo nixos-rebuild switch --flake ~/nixos-config#$(hostname)";
       vim = "nvim";
+      
+      # ls aliases
       ls = "lsd";
+      ll = "lsd -l";
+      la = "lsd -a";
+      lla = "lsd -la";
       tree = "lsd --tree";
+
+      # Quick navigation
+      home = "cd ~";
+      config = "cd ~/nixos-config";
     };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
   };
 }
