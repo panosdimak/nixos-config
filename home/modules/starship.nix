480fc388636a799c8ff44d0f405d17ada5f44eb9
Panos Dimakakos <panosdimak13@gmail.com>
2025-10-06 15:18:34 +0300

    fix: update starship configuration to enhance directory and git status formatting

--- home/modules/starship.nix
+++ home/modules/starship.nix
@@ -7,11 +7,23 @@
       format = "$directory$git_branch$git_status$cmd_duration$fill$cmake$cpp$nix_shell$jobs$line_break$character";
 
       fill.symbol = " ";
+      
+      directory = {
+        truncation_length = 3;
+        truncate_to_repo = true;
+      };
 
+      git_status = {
+        ahead = "⇡\${count}";
+        diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
+        behind = "⇣\${count}";
+      };
+      
       c = {
         style = "";
         format = "[$name $version ]($style)";
       };
+
       nix_shell = {
         style = "bold blue";
         symbol = "❄️";
