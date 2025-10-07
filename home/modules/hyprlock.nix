60f1d3c6140a7cd4942bcf64c380d1338176431d
Panos Dimakakos <panosdimak13@gmail.com>
2025-10-07 19:11:22 +0300

    fix: center align time and date display in hyprlock configuration

--- home/modules/hyprlock.nix
+++ home/modules/hyprlock.nix
@@ -42,18 +42,18 @@
 					text = "$TIME";
 					font_size = 90;
 					font_family = "Monospace";
-					position = "-30, 0";
-					halign = "right";
-					valign = "top";
+					position = "0, 200";
+					halign = "center";
+					valign = "center";
 				}
 				{
 					monitor = "";
 					text = "cmd[update:60000] date +\"%A, %d %B %Y\"";
 					font_size = 25;
 					font_family = "Monospace";
-					position = "-30, -150";
-					halign = "right";
-					valign = "top";
+					position = "0, 100";
+					halign = "center";
+					valign = "center";
 				}
 				{
 					monitor = "";
