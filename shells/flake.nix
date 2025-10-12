7f3c9ec4712a4c02f92d4c0dbd58e7efbe5ff052
Panos Dimakakos <panosdimak13@gmail.com>
2025-10-12 20:55:34 +0300

    feat: add python dev shell

---
+++ shells/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Python dev env";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        (pkgs.python312.withPackages (ps: with ps; [ 
+          numpy
+          matplotlib
+        ]))
+      ];
+
+      shellHook = ''
+          echo "🐍 Welcome to Python devshell"
+      '';
+
+    };
+  };
+}
