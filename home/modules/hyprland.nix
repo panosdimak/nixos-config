a6c281b39f950cd051c2e25772d29bc80a6d64b3
Panos Dimakakos <panosdimak13@gmail.com>
2025-11-06 01:31:08 +0200

    fix: update window border size

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -53,7 +53,7 @@
       general = {
         gaps_in = 5;
         gaps_out = 10;
-        border_size = 4;
+        border_size = 2;
         # "col.active_border" = "rgba(e2bc7bee) rgba(117cb0ee) 45deg";
         # "col.inactive_border" = "rgba(595959aa)";
         resize_on_border = false;
