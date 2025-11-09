fe04dfa4e99adb033dd0b3df3c4bddc72a92b9cd
Panos Dimakakos <panosdimak13@gmail.com>
2025-11-09 20:41:44 +0200

    remove swww input

--- flake.nix
+++ flake.nix
@@ -60,11 +60,6 @@
       url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    swww = {
-      url = "github:LGFae/swww";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   
   outputs = { self, nixpkgs, home-manager, musnix, stylix, zen-browser, apple-fonts, ... }@inputs: {
