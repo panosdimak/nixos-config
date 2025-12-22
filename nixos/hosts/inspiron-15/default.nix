172f622479e71e292ce7b38e74240fb27fd47ede
Panos Dimakakos <panosdimak13@gmail.com>
2025-12-22 15:38:57 +0200

    system/inspiron-15: add usb & bluetooth autosuspend

--- nixos/hosts/inspiron-15/default.nix
+++ nixos/hosts/inspiron-15/default.nix
@@ -16,11 +16,20 @@
   system.stateVersion = "25.05";
   networking.hostName = "inspiron-15";
 
+
+  services.udev.extraRules = ''
+    # Autosuspend webcam
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0c45", ATTR{idProduct}=="6730", TEST=="power/control", ATTR{power/control}="auto"
+
+    # Autosuspend Intel AX201 Bluetooth USB interface
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="8087", ATTR{idProduct}=="0026", TEST=="power/control", ATTR{power/control}="auto"
+  '';
+
   # Choose display manager for this host
   profiles.displayManager = {
     dm = "gdm";
     wayland = true;
   };
 
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/mocha.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
 }
