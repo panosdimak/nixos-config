a828e730a22ec0203acbd3f89e6dd24ccac8dd90
Panos Dimakakos <panosdimak13@gmail.com>
2026-02-10 23:11:04 +0200

    refactor fuzzel configuration: remove unused color settings and streamline structure

--- home/modules/fuzzel.nix
+++ home/modules/fuzzel.nix
@@ -1,23 +1,17 @@
-{ config, lib, ... }:
+{ lib, ... }:
 
-let
-  alpha = "99"; # choose your transparency (00–FF)
-  bg = config.lib.stylix.colors.base00;     # RGB from Stylix theme
-in
 {
   programs.fuzzel = {
     enable = true;
 
     settings = {
-      colors = {
-        background = lib.mkForce "${bg}${alpha}";
-      };
-
       main = {
         font = lib.mkForce "Monospace:size=16";
         dpi-aware = "no";
         use-bold = "yes";
         icon-theme = "Colloid-Dark";
+        # Include matugen-generated colors
+        include = "~/.config/fuzzel/colors.ini";
       };
 
       border = {
