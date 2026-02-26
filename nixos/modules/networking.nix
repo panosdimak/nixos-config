24bf7e777064cda83d1d5b6f98c481cab3a13b7c
Panos Dimakakos <panosdimak13@gmail.com>
2026-02-26 16:11:46 +0200

    nixos/networking: consolidate firewall rules and add comments

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -4,11 +4,11 @@
   networking.networkmanager.enable = true;
 
   networking.firewall = {
+    # LocalSend
     allowedTCPPorts = [ 53317 ];
     allowedUDPPorts = [ 53317 ];
-  };
-  
-  networking.firewall = rec {
+
+    # KDE Connect
     allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
     allowedUDPPortRanges = [{ from = 1714; to = 1764; }];
   };
