1537ff1da8b5cb941a94f1754dfedc59421f0f7e
Panos Dimakakos <panosdimak13@gmail.com>
2026-02-26 15:59:13 +0200

    shells: add rust dev env

---
+++ shells/rust/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.pkg-config
+            pkgs.openssl
+          ];
+
+          RUST_BACKTRACE = 1;
+
+          shellHook = ''
+            export PATH="$HOME/.cargo/bin:$PATH"
+          '';
+        };
+      }
+    );
+}
