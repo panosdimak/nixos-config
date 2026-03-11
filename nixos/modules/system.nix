db229022900de34ec0e494dd9becca0a0f06cd48
Panos Dimakakos <panosdimak13@gmail.com>
2026-03-11 23:25:36 +0200

    system: organize cachix config

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -10,15 +10,19 @@
 
   nix.settings.auto-optimise-store = true;
   # Prefer prebuilt binaries from common caches to avoid local builds
-  nix.settings.substituters = [
-    "https://cache.nixos.org/"
-    "https://nix-community.cachix.org"
-    "https://hyprland.cachix.org"
-  ];
-  nix.settings.trusted-public-keys = [
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-  ];
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+    ];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
+
   # Speed up downloads by allowing more parallel HTTP connections and failing slow mirrors faster
   nix.settings.http-connections = 50;
   nix.settings.connect-timeout = 5;
