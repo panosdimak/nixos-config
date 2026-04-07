91e5fe27d47b8e348754456dcc75f81608ab8d3e
Panos Dimakakos <pdimakakos@proton.me>
2026-04-07 15:35:39 +0300

    lock: update nixpkgs hyprland zen inputs

---
+++ nixos/modules/dms.nix
@@ -0,0 +1,10 @@
+{
+  programs.dms-shell = {
+    enable = true;
+    quickshell.package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
+
+    systemd = {
+      enable = true;             # Systemd service for auto-start
+      restartIfChanged = true;   # Auto-restart dms.service when dms-shell changes
+    };
+}
