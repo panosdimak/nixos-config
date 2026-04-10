f49f2cca23a6032e94f0e7b4a09d83e873abd3ea
Panos Dimakakos <pdimakakos@proton.me>
2026-04-11 02:30:54 +0300

    nixos/music-production: wrap reaper with x11 backend

--- home/modules/music-production.nix
+++ home/modules/music-production.nix
@@ -1,10 +1,22 @@
 { pkgs, ...}:
 
+let
+  # REAPER's SWELL hits BadWindow crashes under XWayland when copying
+  # tracks with recordings; forcing the GDK x11 backend avoids the crash.
+  reaper-x11 = pkgs.symlinkJoin {
+    name = "reaper-x11";
+    paths = [ pkgs.reaper ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/reaper --set GDK_BACKEND x11
+    '';
+  };
+in
 {
   home.packages = with pkgs; [
     wineWow64Packages.staging winetricks mangohud protontricks
     yabridge
     yabridgectl
-    reaper
+    reaper-x11
   ];
 }
