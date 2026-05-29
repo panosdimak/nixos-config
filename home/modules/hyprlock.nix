1ff17bdb8ae536f06ee17460fc12b9c5cb987c02
Panos Dimakakos <pdimakakos@proton.me>
2026-05-30 00:04:06 +0300

    home/hyprlock: route hypridle lock_cmd to DMS

--- home/modules/hyprlock.nix
+++ home/modules/hyprlock.nix
@@ -5,7 +5,7 @@
 		enable = true;
 		settings = {
 			general = {
-				lock_cmd = "pidof hyprlock || hyprlock";
+				lock_cmd = "dms ipc call lock lock";
 				inhibit_sleep = 3;
 				before_sleep_cmd = "loginctl lock-session";
 				after_sleep_cmd = "hyprctl dispatch dpms on";
