532f13eef1c0f7abcf379fb2e3677df578301724
Panos Dimakakos <pdimakakos@proton.me>
2026-05-30 15:56:50 +0300

    nixos/dms: remove env var

--- nixos/modules/dms.nix
+++ nixos/modules/dms.nix
@@ -16,10 +16,4 @@
       restartIfChanged = true; # Auto-restart dms.service when dms-shell changes
     };
   };
-
-  # Use exclusive keyboard focus on the layer surface instead of HyprlandFocusGrab.
-  # Why: workspace dispatches from the overview break the grab, which closes the
-  # overlay mid-interaction (arrows stop working after one press, Escape can't
-  # fire). See HyprlandOverview.qml + CompositorService.qml in dms-shell.
-  systemd.user.services.dms.environment.DMS_HYPRLAND_EXCLUSIVE_FOCUS = "1";
 }
