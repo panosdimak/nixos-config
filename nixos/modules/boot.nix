a2e631b9c59040075a89ecb82d2f19a3ceb6a50d
Panos Dimakakos <pdimakakos@proton.me>
2026-06-02 14:59:49 +0300

    nixos/boot: keep 5 boot generations for rollback headroom

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -6,7 +6,7 @@
     consoleMode = "keep";
   };
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 3;
+  boot.loader.systemd-boot.configurationLimit = 5;
 
   boot.initrd.systemd.enable = true;
   boot.initrd.kernelModules = [ "lz4" ];
